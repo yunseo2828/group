904d49d24817bcfa41ce6fcc7f6b355658cfd398
wanya <cys567599@gmail.com>
2021-06-07 16:18:03 +0900

    Initial commit

---
+++ src/main/java/itwill/group/SpringProjectApplication.java
@@ -0,0 +1,15 @@
+package itwill.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class })
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
